790408ab95f090a68e40c723899f56d560c9530e
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-11-25 21:55:59 +0900

    useEffect로 json 데이터 불러오기 / tanstackquery 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <>app</>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/todos")
+      .then((response) => {
+        console.log(response);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return <>{JSON.stringify(data)}</>;
 }
 
 export default App;
